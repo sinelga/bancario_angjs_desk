2e5ad4f43163658b636b3f46e7fd77b9cc9c9485
juno <juno@krasava.fi>
2014-07-12 08:29:16 +0300

    prod

--- bancario_angjs_desk/app/js/app.js
+++ bancario_angjs_desk/app/js/app.js
@@ -4,24 +4,5 @@
 var app = angular.module('bancario_angjs_desk', ['controllers']);
 
 
-
-//app.run('LoadCtrl');
-
-//app.controller('LoadCtrl', function ($scope,$http) {
-//	
-//	
-//	var url = "http://146.185.151.26/redis?redisid=it_IT:news:Home&callback=JSON_CALLBACK";
-//	
-//	$http.jsonp(url).success(function(data){
-//		
-//		 $scope.rss=data;
-//		
-//
-//		
-//	});
-//	
-//	
-//	
-//});
  
 
--- bancario_angjs_desk/app/js/controllers.js
+++ bancario_angjs_desk/app/js/controllers.js
@@ -3,22 +3,34 @@ var app = angular.module('controllers', []);
 
 app.controller('LoadCtrl', function ($scope,$http) {
 	
+	$scope.clickOnRss= false;
+	
+	
 	
 	var url = "http://146.185.151.26/redis?redisid=it_IT:news:Home&callback=JSON_CALLBACK";
 	
 	$http.jsonp(url).success(function(data){
 		
 		 $scope.rss=data;
+//		 $scope.selectedRss =data[data.length -1];
 		
 
 		
 	});
 	
-	
-	
+		
 	$scope.selectRss = function(rs) {
 		
-		console.log(rs.Title)
+		
+		$scope.clickOnRss= true;
+		$scope.selectedItem = rs;
+				
+		
+	}
+	
+	$scope.closeItem = function() {
+		
+		$scope.clickOnRss= false;
 		
 	}
 	
